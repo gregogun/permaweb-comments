1e2a9f70b13426ac78b4267ce246999c67541079
gregogun <gregogun97@gmail.com>
2023-06-04 07:16:50 +0100

    chore: update test tx

--- config.ts
+++ config.ts
@@ -6,5 +6,5 @@ export const config = {
   boringAvatars: "https://source.boringavatars.com/marble",
   accountAvatarDefault:
     "https://hky34zdnsy3xyhltbpuy6plcqt33qmenxbabevfgyjdcdux444aa.arweave.net/OrG-ZG2WN3wdcwvpjz1ihPe4MI24QBJUpsJGIdL85wA",
-  testSourceTx: "-MHHHVNkpvBApY8MO6h15dTjQ0loapghv5dgN1SLU4Y",
+  testSourceTx: "oS9tolPhr8fvFEDkvcSoB9th6Ar2dToi7h8HM-jjmyE",
 };
